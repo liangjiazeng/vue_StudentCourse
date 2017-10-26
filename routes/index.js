061a932991f9b3325a2ff32fe1beaad8f7b92e85
plusyu <809501557@qq.com>
2017-10-26 12:20:13 +0800

    2.0

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("<h1>welcome to express</h1>");
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
